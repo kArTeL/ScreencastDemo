2651212c630f5be95f6b49b2450e3197c91678d0
Neil <neilliga@gmail.com>
2021-03-26 17:20:18 -0600

    Removed pods

--- Pods/Target Support Files/Pods-ScreenCastDemo-ScreenCastDemoUITests/Pods-ScreenCastDemo-ScreenCastDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ScreenCastDemo_ScreenCastDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ScreenCastDemo_ScreenCastDemoUITests
-@end
--- Pods/Target Support Files/Pods-ScreenCastDemo/Pods-ScreenCastDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ScreenCastDemo : NSObject
-@end
-@implementation PodsDummy_Pods_ScreenCastDemo
-@end
